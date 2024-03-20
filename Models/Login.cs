49bcc9f3cd3da5eae610f0db56bd37bbf3bb8ee4
akashgautamm <gautama55696@gmail.com>
2024-03-20 18:01:44 +0530

    Registration create

--- Models/Login.cs
+++ Models/Login.cs
@@ -1,6 +1,31 @@
-﻿namespace Train_Management_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Train_Management_System.Models
 {
     public class Login
     {
+        // -- Validating Student Name  
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(12)]
+        public string Name { get; set; }
+        // -- Validating Email Address  
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+        // -- Validating Contact Number  
+        [Required(ErrorMessage = "Contact is required")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid Phone number")]
+        public string Contact { get; set; }
+        // -- Validating Password
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password{get;set; }
+        // -- Validating Confirm Password
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword{get;set;}
     }
+
 }
